76eecbb3ddcafff913239a29615c07c1b8d56a1d
harsh-0706 <hp99harsh@gmail.com>
2022-04-10 00:00:45 +0530

    Commit till lecture 20

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -13,10 +14,21 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
         inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public String getTitle() {
         return title;
@@ -45,10 +57,9 @@ public class Book {
         this.authors = authors;
     }
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-        this.authors = authors;
     }
 
     @Override
